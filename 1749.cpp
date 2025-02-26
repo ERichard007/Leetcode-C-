9c5f23b4f0039e954a8228e479fe7fc2e973c76d
ERichard007 <ecrb29@umsystem.edu>
2025-02-26 12:13:25 -0600

    1749 DONE

--- 1749.cpp
+++ 1749.cpp
@@ -6,7 +6,21 @@ using namespace std;
 class Solution {
     public:
         int maxAbsoluteSum(vector<int>& nums) {
-            
+            int maxSum = nums[0];
+            int resSum = nums[0];
+            for (int i = 1; i < nums.size(); i++){
+                maxSum = max(maxSum + nums[i], nums[i]);
+                resSum = max(maxSum, resSum);
+            }
+
+            int minSum = nums[0];
+            int resSum2 = nums[0];
+            for (int i = 1; i < nums.size(); i++){
+                minSum = min(minSum + nums[i], nums[i]);
+                resSum2 = min(minSum, resSum2);
+            }
+
+            return max(resSum, abs(resSum2));
         }
     };
 
